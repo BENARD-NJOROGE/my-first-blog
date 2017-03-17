c8383d65a37b14126aea1fa2539ca0813cbb41b8
benard <kimnjoro@gmail.com>
2017-03-17 09:52:53 +0300

    update files

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .forms import PostForm
+
+
 urlpatterns = [
-     url(r'^$',views.post_list, name='post_list')
-     ]
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+]
